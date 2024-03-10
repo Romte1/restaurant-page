4934919eacf9afdf48611fd044166958eca985a0
Romte1 <ivan_ni224@hotmail.com>
2024-03-09 23:33:27 -0300

    Added more html using javascript instead of plain html

--- src/index.js
+++ src/index.js
@@ -8,17 +8,24 @@ function home() {
     const headH1 = document.createElement('h1');
     const bodyTitle = document.createElement('p');
     const bodySchedule = document.createElement('h2');
-    const bodyDayMon = document.createElement('p');
+    const days = document.createElement('div');
 
     headH1.textContent = 'Welcome to this restaurant!';
     bodyTitle.textContent = 'We are a restaurant that loves everything about food, and we try our best so you leave this place loving it as much as we do!';
     bodySchedule.textContent = 'Schedule';
-    bodyDayMon.textContent = 'Mon 9am-10pm';
+    days.innerHTML = 
+    `<p>Mon 9am-10pm</p>
+    <p>Thu 9am-10pm</p>
+    <p>Wed 9am-10pm</p>
+    <p>Tur 9am-10pm</p>
+    <p>Fri 9am-10pm</p>
+    <p>Sat 9am-10pm</p>
+    <p>Sun 10am-3pm</p>`;
 
     contentHead.appendChild(headH1);
     contentBody.appendChild(bodyTitle);
     contentBody.appendChild(bodySchedule);
-    contentBody.appendChild(bodyDayMon);
+    contentBody.appendChild(days);
 
     
     content.appendChild(contentHead);
