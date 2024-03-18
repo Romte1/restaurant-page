9aece3a6dd8bf9cf19212d1d298aad6ebf3acb9d
Romte1 <ivan_ni224@hotmail.com>
2024-03-17 23:08:46 -0300

    Added more text to the about aka contact button showing in the index page when clicking it

--- src/contact.js
+++ src/contact.js
@@ -1,16 +1,23 @@
 const createContactPage = () => {
     const content = document.querySelector('#content');
-    const contactUs = document.createElement('h2');
-    const address = document.createElement('h2');
+    const contactUs = document.createElement('h1');
     const phone = document.createElement('h2');
     const email = document.createElement('h2');
-    const operationHours = document.createElement('h2');
     const reservations = document.createElement('h2');
     const generalInquiries = document.createElement('h2');
+    const pContactUs = document.createElement('p');
+    const pPhone = document.createElement('p');
 
     contactUs.textContent = 'Contact Us';
+    pContactUs.textContent = "Thank you for your interest in this restaurant!. We're thrilled to hear from you!";
+    phone.textContent = 'Phone:';
+    pPhone.textContent = '(555) 123-4567';
+
 
     content.appendChild(contactUs);
+    content.appendChild(pContactUs);
+    content.appendChild(phone);
+    content.appendChild(pPhone);
 
 
 
