8b12920fb8cc3830ec3e177c42dcc6ef5c23c1fd
Romte1 <ivan_ni224@hotmail.com>
2024-03-27 22:40:27 -0400

    Added text to menu section

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,13 @@
 const createMenuPage = () => {
     const content = document.querySelector('#content');
     const title = document.createElement('h1');
+    const beverages = document.createElement('h2');
 
     title.textContent = 'Menu';
+    beverages.textContent = 'Beverages'
 
     content.appendChild(title);
+    content.appendChild(beverages);
 
 
 }
